b54521f468e9d4bae34d25e532590b0f5493e9a0
Sherry <71168826+ShilunDai@users.noreply.github.com>
2025-06-02 21:04:19 -0400

    LC-binary-search [Runtime: 0 ms; Memory: 31.4 MB]

--- binary-search/binary-search.cpp
+++ binary-search/binary-search.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int left = 0;
-        int right = nums.size() - 1;
+        int right = nums.size();
 
-        while (left <= right){
+        while (left < right){
             int middle = (right + left) / 2;
 
             if (target < nums[middle]){
-                right = middle - 1;
+                right = middle;
             }else if (target > nums[middle]){
                 left = middle + 1;
             }else{return middle;}
