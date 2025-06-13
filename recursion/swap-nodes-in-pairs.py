f8cae68cc57c3784de0b5ce802a855de98847137
Sherry <71168826+ShilunDai@users.noreply.github.com>
2025-06-13 18:01:07 -0400

    LC-swap-nodes-in-pairs [Runtime: 0 ms; Memory: 17.9 MB]

--- recursion/swap-nodes-in-pairs.py
+++ recursion/swap-nodes-in-pairs.py
@@ -1,27 +1,22 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def swapPairs(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummyhead = ListNode()
         dummyhead.next = head
-        curr = head
         pre = dummyhead
+        curr = head
 
         while curr and curr.next:
             temp = curr.next
             pre.next = temp
             curr.next = temp.next
             temp.next = curr
+
             pre = curr
             curr = pre.next
 
         return dummyhead.next
-        
