b310e8a38671bbd075ba853d187580d1db4fb75e
Sherry <71168826+ShilunDai@users.noreply.github.com>
2025-06-13 20:42:04 -0400

    LC-reverse-linked-list [Runtime: 0 ms; Memory: 18.5 MB]

--- linked-list/reverse-linked-list.py
+++ linked-list/reverse-linked-list.py
@@ -1,21 +1,16 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         pre = None
         curr = head
 
         while curr:
             temp = curr.next
             curr.next = pre
-
             pre = curr
             curr = temp
 
