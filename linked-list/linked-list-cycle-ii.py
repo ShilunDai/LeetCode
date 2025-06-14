8851b94437fe9cff9af9676e844db0bfee80a64c
Sherry <71168826+ShilunDai@users.noreply.github.com>
2025-06-13 20:24:39 -0400

    LC-linked-list-cycle-ii [Runtime: 43 ms; Memory: 19.7 MB]

--- linked-list/linked-list-cycle-ii.py
+++ linked-list/linked-list-cycle-ii.py
@@ -1,26 +1,23 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def detectCycle(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        fast = head
-        slow = head
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummyhead = ListNode()
+        dummyhead.next = head
+        slow = dummyhead
+        fast = dummyhead
 
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
 
-            if fast == slow:
-                index1 = head
-                index2 = fast
-
+            if fast==slow:
+                index1=fast
+                index2=dummyhead
                 while index1 != index2:
                     index1 = index1.next
                     index2 = index2.next
