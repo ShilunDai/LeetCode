14a2e9f57b4cfe2dcbbc0ebf2f5e2a75844d605f
Sherry <71168826+ShilunDai@users.noreply.github.com>
2025-07-01 20:00:23 -0400

    LC-remove-linked-list-elements [Runtime: 3 ms; Memory: 19.5 MB]

--- linked-list/remove-linked-list-elements.py
+++ linked-list/remove-linked-list-elements.py
@@ -1,23 +1,17 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def removeElements(self, head, val):
-        """
-        :type head: Optional[ListNode]
-        :type val: int
-        :rtype: Optional[ListNode]
-        """
-        dummyhead = ListNode()
-        dummyhead.next = head
-        curr = dummyhead
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode()
+        dummy.next = head
+        curr = dummy
 
         while curr.next:
             if curr.next.val == val:
                 curr.next = curr.next.next
             else:
                 curr = curr.next
-
-        return dummyhead.next
+        return dummy.next
