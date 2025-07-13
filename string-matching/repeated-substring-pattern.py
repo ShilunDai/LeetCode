9a1e6ea0d8ba0a0a52eca638c2c0440dfd0c6c63
Sherry <71168826+ShilunDai@users.noreply.github.com>
2025-07-12 22:31:38 -0400

    LC-repeated-substring-pattern [Runtime: N/A; Memory: N/A]

---
+++ string-matching/repeated-substring-pattern.py
@@ -0,0 +1,12 @@
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        next = [0] * len(s)
+        j = 0
+
+        for i in range(1, len(s)):
+            while j>0 and s[i]!=s[j]:
+                j=next[j-1]
+            if s[i]==s[i]:
+                j+=1
+            next[i]=j
+        return len(s)%(len(s)-next[-1])==0
