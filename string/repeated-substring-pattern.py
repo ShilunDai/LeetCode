531558bede6a1a7e6111bb28f347bdfced6552de
Sherry <71168826+ShilunDai@users.noreply.github.com>
2025-07-12 22:58:04 -0400

    LC-repeated-substring-pattern [Runtime: 43 ms; Memory: 18.2 MB]

--- string/repeated-substring-pattern.py
+++ string/repeated-substring-pattern.py
@@ -6,9 +6,7 @@ class Solution:
         for i in range(1, len(s)):
             while j>0 and s[i]!=s[j]:
                 j=next[j-1]
-            if s[i]==s[i]:
+            if s[i]==s[j]:
                 j+=1
             next[i]=j
-        if next[-1] != 0 and len(s) % (len(s) - next[-1]) == 0:
-            return True
-        return False
+        return next[-1] != 0 and len(s) % (len(s) - next[-1]) == 0
