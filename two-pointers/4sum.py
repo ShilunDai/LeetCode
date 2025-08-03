0f367e3c5b8b796033d318211f302ae917dc0f29
Sherry <71168826+ShilunDai@users.noreply.github.com>
2025-08-03 19:40:11 -0400

    LC-4sum [Runtime: 394 ms; Memory: 17.9 MB]

--- two-pointers/4sum.py
+++ two-pointers/4sum.py
@@ -9,7 +9,7 @@ class Solution:
             if i>0 and nums[i]==nums[i-1]:
                 continue
             for j in range(i+1, len(nums)):
-                if nums[j]>target and target>=0:
+                if nums[i]+nums[j]>target and target>=0:
                     break
                 if j>i+1 and nums[j]==nums[j-1]:
                     continue
