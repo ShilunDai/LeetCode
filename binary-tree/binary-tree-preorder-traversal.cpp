ea5e2d78a11aef75f29e1de5f117e89f504fe66b
Sherry <71168826+ShilunDai@users.noreply.github.com>
2025-08-04 17:58:17 -0400

    LC-binary-tree-preorder-traversal [Runtime: 0 ms; Memory: 11 MB]

---
+++ binary-tree/binary-tree-preorder-traversal.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void traversal(TreeNode* curr, vector<int>& vec){
+        if (curr == NULL){
+            return;
+        }
+        vec.push_back(curr->val);
+        traversal(curr->left, vec);
+        traversal(curr->right, vec);
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        traversal(root, result);
+        return result;
+    }
+};
